8b3d4d190287e9fc936c491a0a865c3dd616667d
Precious Praise <akatukundapp@gmail.com>
2025-11-10 09:08:04 +0300

    logo

--- app.py
+++ app.py
@@ -29,10 +29,12 @@ st.set_page_config(
 # -----------------------------
 # App Header
 # -----------------------------
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("AILOGO.png", width=80)
 st.markdown(
     """
-    <div style='text-align: center;'>
-        <img src="AILOGO.png" width="80">
+    <div style='text-align: center;'>       
         <h1 style='color: #1f77b4;'>InjuryGuard AI</h1>
         <p style='font-size:18px;'>AI-powered Real-time Athlete Injury Monitoring Platform</p>
     </div>
